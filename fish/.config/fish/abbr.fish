9d66721934a7f6106b786b53c07387b23bb87289
Ansh2209 <anshraj220109@proton.me>
2026-05-02 18:19:01 +0530

    feat: add fish package

---
+++ fish/.config/fish/abbr.fish
@@ -0,0 +1,18 @@
+# CLI
+abbr -a cat 'bat'
+abbr -a .. 'cd ..'
+abbr -a ls 'eza -hal --color=auto --icons=auto --group-directories-first --no-user --no-permissions'
+abbr -a tree 'eza -Ta'
+abbr -a find 'fd'
+abbr -a cl 'clear'
+abbr -a lg 'lazygit'
+abbr -a du 'gdu'
+abbr -a y 'yazi'
+abbr -a g 'git'
+
+# ARCH
+abbr -a cleanup 'sudo pacman -Rns (pacman -Qtdq)'
+
+# Fish
+abbr -a fishs 'source ~/.config/fish/config.fish'
+abbr -a abbrs 'micro ~/.config/fish/abbr.fish'
---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,20 @@
+if status is-interactive
+    set -g fish_greeting "" # REMOVE FISH GREETING
+
+    # SOURCE
+    source ~/.config/fish/abbr.fish
+    source ~/.config/fish/functions.fish
+        
+    # INITIALIZE TOOLS
+    thefuck --alias | source
+    zoxide init fish | source
+    mise activate fish | source
+end
+
+if status --is-login
+    if test (tty) = "/dev/tty1"
+        if uwsm check may-start
+            exec uwsm start hyprland.desktop
+        end
+    end
+end
