c4fddfaa308de4d9c9a0b4860a072f62ab683aa2
Ansh2209 <anshraj220109+github@proton.me>
2026-05-13 21:16:24 +0530

    chore: add window_rule for xdg-desktop-portal-gtk
    to set a fixed size and centered layout

--- hyprland-uwsm/.config/hypr/hyprland/window-rules.lua
+++ hyprland-uwsm/.config/hypr/hyprland/window-rules.lua
@@ -3,3 +3,11 @@ hl.window_rule({
     match = { class = ".*" },
     suppress_event = "maximize",
 })
+
+hl.window_rule({
+    name = "Center and size xdg-desktop-portal-gtk",
+    match = { initial_class = "xdg-desktop-portal-gtk" },
+    float = true,
+    center = true,
+    size = {800, 600},
+})
